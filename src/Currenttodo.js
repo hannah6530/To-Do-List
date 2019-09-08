181386f6dbe772a9b0c3f22f9205abeb3f550604
“hannah6530” <hannah6530@gmail.com>
2019-09-07 22:35:25 -0400

    first commit

---
+++ src/Currenttodo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Currenttodo = (props) => {
+
+  return(
+    <div id="list">
+      <h2>My Todos</h2>
+      <ul id="tasks">
+      </ul>
+    </div>
+  )
+
+}
+export default Currenttodo;
---
+++ src/Todoform.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Currenttodo from './Currenttodo'
+
+const Todoform = () => {
+
+  return(
+
+    <form id="create-task-form" action="#" method="post">
+      <label for="new-task-description">Task description:</label>
+      <input type="text" id="new-task-description" name="new-task-description" placeholder="description" />
+      <input type="submit" value="Create New Task" />
+    </form>
+
+  )
+
+}
+export default Todoform;
